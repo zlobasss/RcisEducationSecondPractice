b79ddde9d3177040ee9e9886fc3388febf16a4f0
Злобин Сергей Сергеевич <zlobin.s2005@mail.ru>
2023-03-22 12:21:56 +0700

    new Commit :#

---
+++ PracticWork2/PracticWork2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
